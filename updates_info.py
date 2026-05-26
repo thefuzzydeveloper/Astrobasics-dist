ca1bd1c7ee5a53e60f747e4723e84c801d99f742
Alhmora <morakweenie@gmail.com>
2026-05-26 15:17:47 +0530

    v1.3.3

--- updates_info.py
+++ updates_info.py
@@ -1197,3 +1197,26 @@ v1_3_23= """
 		[+]</span></b>Enhances reverse search module for even faster results!<br>
 </div>
 """
+
+
+v1_3_3= """
+<div style="font-family: 'Segoe UI', system-ui, sans-serif;">
+	<h2 style="color: #2563EB; margin-bottom: 5px; margin-top: 0;">
+		AstroBasics Diamond Chart Pro - What's changed?</h2>
+	<hr style="background-color: #CBD5E1; border: none; height: 1px; margin-bottom: 12px;">
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>Even better exports, removed unnecessary metadata files form site packages!<br>
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>Minor fix in panchang from main app view, which was showing wrong tithis.<br>
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>Added new tabs in Detailed analysis, currently experimental!<br>
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>Minor fixes in muhurta module!<br>
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>Enhances reverse search module for even faster results!<br>
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>Improved on the fly transit calculations monumentally now it is highly configurable!<br>
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>The whole app is even faster now with multiprocessing support!<br>
+</div>
+"""
