607db4538e25fdfd834d3c0414263281ebe7f6d6
Alhmora <morakweenie@gmail.com>
2026-05-27 09:34:43 +0530

    v1.3.4

--- updates_info.py
+++ updates_info.py
@@ -1220,3 +1220,14 @@ v1_3_3= """
 		[+]</span></b>Added multiprocessing support for heavy calculations!<br>
 </div>
 """
+
+
+v1_3_4= """
+<div style="font-family: 'Segoe UI', system-ui, sans-serif;">
+	<h2 style="color: #2563EB; margin-bottom: 5px; margin-top: 0;">
+		AstroBasics Diamond Chart Pro - What's changed?</h2>
+	<hr style="background-color: #CBD5E1; border: none; height: 1px; margin-bottom: 12px;">
+	<p style="font-size: 13px; color: #475569; ">
+		[+]</span></b>Added planetary aspects in detailed report!<br>
+</div>
+"""
