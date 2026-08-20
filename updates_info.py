20b31050ad007d58ca1a27777ab6faad54658b7f
The Developer <morakweenie@gmail.com>
2026-08-20 11:56:23 +0530

    v1.4.003

--- updates_info.py
+++ updates_info.py
@@ -2443,3 +2443,21 @@ v1_4_002 = """
 """
 
 
+
+v1_4_003 = """
+<div style="font-family: 'Segoe UI', Arial, sans-serif; color: #334155; line-height: 1.5; font-size: 13px;">
+	<h2 style="color: #2563EB; margin-bottom: 5px; margin-top: 0;">What's Changed?</h2>
+	<hr style="background-color: #CBD5E1; border: none; height: 1px; margin-bottom: 12px;">
+
+	<p style="margin-bottom: 14px;">
+		<b style="color: #059669;">[+]</b> <b>Added Medical Astrology Module</b><br>
+		<b style="color: #059669;">[+]</b> <b>Added One liner results!</b><br>
+		<b style="color: #059669;">[+]</b> <b>Added Clear logs button in logger!</b><br>
+		<b style="color: #059669;">[+]</b> <b>Brand New Plugin System:</b> You can now write custom tools with zero boilerplate or even in a single line of code!
+		<b style="color: #059669;">[+]</b> <b>Added plugin development studio:</b> Plugin creation is now self-contained in app with auto complete and suggestion of available keywords making plugin creation intuitive and fun experience!
+		<b style="color: #059669;">[+]</b> <b>Added Shorcuts:</b> Added common shortcuts for easy chart management like Ctrl+S for saving chart or Ctrl + N for launching new window!
+	</p>
+</div>
+"""
+
+
