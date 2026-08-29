efab2159155599f9aa102e993512b68cefcbc354
The Developer <morakweenie@gmail.com>
2026-08-29 20:13:45 +0530

    v1.4.107

--- updates_info.py
+++ updates_info.py
@@ -23,3 +23,17 @@ v1_4_106 = """
 		</p>
 	</div>
 """
+
+
+v1_4_107 = """
+	<div style="font-family: 'Segoe UI', Arial, sans-serif; color: #334155; line-height: 1.5; font-size: 13px;">
+		<h2 style="color: #2563EB; margin-bottom: 5px; margin-top: 0;">Whats changed? MAJOR UPDATE!</h2>
+		<hr style="background-color: #CBD5E1; border: none; height: 1px; margin-bottom: 12px;">
+
+		<p style="margin-bottom: 14px;">
+			<b style="color: #059669;">[+]</b> <b>Major Bug fixes and Better UI layout, Major code transformation and tab workspace stability!</b><br>
+			<b style="color: #059669;">[+]</b> <b>Encryption Bug fixes!</b><br>
+			<b style="color: #059669;">[+]</b> <b>Translation Bug fixes!</b><br>
+		</p>
+	</div>
+"""
