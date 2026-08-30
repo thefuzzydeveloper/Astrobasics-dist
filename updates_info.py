6bac870fa14bf8c455edb1243a7908af7f02de28
The Developer <morakweenie@gmail.com>
2026-08-30 13:09:31 +0530

    v1.4.109

--- updates_info.py
+++ updates_info.py
@@ -52,3 +52,19 @@ v1_4_108 = """
 		</p>
 	</div>
 """
+
+v1_4_109 = """
+	<div style="font-family: 'Segoe UI', Arial, sans-serif; color: #334155; line-height: 1.5; font-size: 13px;">
+		<h2 style="color: #2563EB; margin-bottom: 5px; margin-top: 0;">Whats changed?</h2>
+		<hr style="background-color: #CBD5E1; border: none; height: 1px; margin-bottom: 12px;">
+
+		<p style="margin-bottom: 14px;">
+			<b style="color: #059669;">[+]</b> <b>Major Bug fixes and Better UI layout, Major code transformation and tab workspace stability!</b><br>
+			<b style="color: #059669;">[+]</b> <b>Encryption Bug fixes!</b><br>
+			<b style="color: #059669;">[+]</b> <b>Translation Bug fixes!</b><br>
+			<b style="color: #059669;">[+]</b> <b>Plugin cache retention!</b><br>
+			<b style="color: #059669;">[+]</b> <b>Added House vitality mode, Now check house afflictions within chart itself! works on four layers!</b><br>
+		</p>
+	</div>
+"""
+
